0df9a5ff9e53ba1e0ae6c9f287db918ab6342c28
wss1996 <wss9642@163.com>
2019-07-13 12:06:49 +0800

    再一次更改配置

--- blog/settings.py
+++ blog/settings.py
@@ -146,7 +146,13 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = '/home/wss/sites/shuangblog.com/myblog'
+
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
