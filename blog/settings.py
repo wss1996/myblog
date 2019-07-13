bb52e211e7dd411f415dfdacbab0d2ede393e893
wss1996 <wss9642@163.com>
2019-07-13 18:04:47 +0800

    再一次更改配置

--- blog/settings.py
+++ blog/settings.py
@@ -150,8 +150,8 @@ STATIC_URL = '/static/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = '/home/wss/sites/shuangblog.com/myblog/static'
-
+# STATIC_ROOT = '/home/wss/sites/shuangblog.com/myblog/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'nginx_static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
